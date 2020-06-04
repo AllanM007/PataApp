7ca4ab7865362a860b3fc7a5fdb2b002dcfb7abf
AllanM007 <no-reply>
2020-06-04 15:32:59 +0300

    add gitignore

--- nearbyshops/shops/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.gis.admin import OSMGeoAdmin
-from django.contrib import admin
-from shops.models import Shop
-
-@admin.register(Shop)
-class ShopAdmin(OSMGeoAdmin):
-    list_display = ('name', 'location')
