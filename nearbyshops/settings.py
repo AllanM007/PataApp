45e5dc3c66bc672372d3c68f3616e7d8928bbf60
AllanM007 <no-reply>
2020-06-04 21:27:42 +0300

    add channel layer

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -78,6 +78,15 @@ WSGI_APPLICATION = 'nearbyshops.wsgi.application'
 
 ASGI_APPLICATION = 'nearbyshops.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
