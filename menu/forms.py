f2d3a4a11e615fa2218fab7cb0b2d5af0a25c8c0
AllanM007 <mwaranguallan345@gmail.com>
2020-06-06 23:50:31 +0300

    add menu forms

---
+++ menu/forms.py
@@ -0,0 +1,24 @@
+from django .form import ModelForm
+from .models import Pizza, Topping, Sauce
+from django.contrib.auth.models import User
+from django import forms
+
+class PizzaForm(forms.ModelForm):
+
+    class Meta:
+        model = Pizza
+        fields = ['name']
+
+class ToppingForm(forms.ModelForm):
+    name = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.RadioSelect())
+
+    class Meta:
+        model = Topping
+        fields = ['pizza']
+
+class SauceForm(forms.ModelForm):
+    name = forms.ChoiceField(choices=RATING_CHOICES, widget=forms.RadioSelect())
+
+    class Meta:
+        model = Sauce
+        fields = ['pizza']
