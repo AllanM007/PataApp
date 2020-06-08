83399949b6c6a54a1f786af8c0f5324c02c2fe11
AllanM007 <mwaranguallan345@gmail.com>
2020-06-08 15:08:40 +0300

    add topping & sauce forms

--- menu/forms.py
+++ menu/forms.py
@@ -4,20 +4,25 @@ from django.contrib.auth.models import User
 from django import forms
 
 TOPPING_CHOICES = [
-    ('MR', 'Mr.'),
-    ('MRS', 'Mrs.'),
-    ('MS', 'Ms.'),
+    ('Pepperoni', 'Pepperoni'),
+    ('Pineapple', 'Pineapple'),
+    ('BBQ Meat', 'BBQ. Meat'),
+    ('Chicken', 'Chicken'),
+    ('Mushrooms', 'Mushrooms'),
 ]
 
 SAUCE_CHOICES = [
-    ('MR', 'Mr.'),
-    ('MRS', 'Mrs.'),
-    ('MS', 'Ms.'),
+    ('BBQ Sauce', 'BBQ Sauce'),
+    ('Ranch Sauce', 'Ranch Sauce'),
+    ('Garlic Sauce', 'Garlic Sauce'),
+    ('Marinara Sauce', 'Marinara Sauce'),
+    ('Hot Sauce', 'Hot Sauce'),
 ]
 
 class PizzaForm(forms.ModelForm):
-	#name = forms.ChoiceField(choices=TOPPING_CHOICES, widget=forms.RadioSelect())
+	topping = forms.ChoiceField(choices=TOPPING_CHOICES, widget=forms.RadioSelect())
+	sauce = forms.ChoiceField(choices=SAUCE_CHOICES, widget=forms.RadioSelect())
 
-    class Meta:
-        model = Pizza
-        fields = ['name']
+	class Meta:
+		model = Pizza
+		fields = ['name']
--- menu/views.py
+++ menu/views.py
@@ -13,15 +13,13 @@ def menu(request):
 
 def pizza(request, pk):
 
-	t_form = ToppingForm()
-	s_form = SauceForm()
+	form = PizzaForm()
 
 	pizza = Pizza.objects.get(pk = pk)
 	
 	context = {
         'pizza':pizza,
-        't_form':t_form,
-        's_form':s_form,
+        'form':form,
     }
 
 	return render(request, 'menu/pizza.html', context)
